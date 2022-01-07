5e6a5978960c3fb1e0cdd47aecf9b462daf90ead
Daniel Weppeler <daniel.weppeler@microsoft.com>
2022-01-07 14:46:16 +0100

    First commit

---
+++ FSLogix/Get-FSLogix-DiskUsage.ps1
@@ -0,0 +1,14 @@
+﻿$VHDOpenedFilePathProfile = (Get-ItemProperty -Path HKCU:\SOFTWARE\FSLogix\Profiles\Session\ -Name VHDOpenedFilePath).VHDOpenedFilePath
+$VHDOpenedFilePathODFC = (Get-ItemProperty -Path HKCU:\SOFTWARE\FSLogix\ODFC\Session\ -Name VHDOpenedFilePath).VHDOpenedFilePath
+$VHDDisks = @($VHDOpenedFilePathProfile,$VHDOpenedFilePathODFC)
+
+Foreach ($d in $VHDDisks) {
+$VHDDisk = Get-DiskImage -ImagePath $d | Select-Object FileSize,Size,ImagePath
+$FileSize = ($VHDDisk.FileSize)/1gb
+$Size = ($VHDDisk.Size)/1gb
+$Usage = ($FileSize/$Size).tostring("P")
+Write-Host("Disk: " + (Split-Path ($VHDDisk.ImagePath) -leaf)) -ForegroundColor Cyan
+Write-Host("Size on Disk(GB): " + $FileSize) -ForegroundColor Cyan
+Write-Host("Disk Usage: " + $Usage) -ForegroundColor Cyan
+Write-Host
+}
